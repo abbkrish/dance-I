12db747852a62c90d32d5cd77d7f8acd757e2ff3
Abishek <abbkrish@gmail.com>
2014-02-09 16:07:12 -0600

    asdf

--- Openglprog2.py
+++ Openglprog2.py
@@ -4,64 +4,95 @@ from OpenGL.GLU import *
 from OpenGL.GLUT import *
 
 import sys
+nFPS=30
+RotateAngle=0.0
+
+
 def init():
     glClearColor(0.1, 0.2, 0.1, 1.0) #RGBA
     gluOrtho2D(-150.0, 150.0, -150.0, 150.0)
 
-def plotpoints():
+def keyboard(key,x,y):
+    if (key=='d'):#dance
+        ######
+        1==1
+    elif (key=='l'):
+        glPolygonMode(GL_FRONT_AND_BACK,GL_LINE)
+        plotpoints(GL_TRIANGLE_FAN)
+    elif (key=='f'):
+        glPolygonMode(GL_FRONT_AND_BACK,GL_FILL)
+        plotpoints(GL_LINE_LOOP)
+    
+
+#def Style():
+ #   if (
+
+def plotpoints(style=GL_LINE_LOOP):
     glClear(GL_COLOR_BUFFER_BIT)
     glColor3f(0.0, 1.2 ,0.2)
     #PLOT I
-    glBegin(GL_TRIANGLE_FAN)
-    #glEnable(GL_BLEND)
-    #glBlendFunc(0.8,0.2)
-    #1st triangle-top left corner
-    glVertex2f(-40,60) #vertex 3
-    glVertex2f(-60,60) #vertex 2
-    glVertex2f(-60,80) #vertex 1
-    glColor3f(1.0,0.0,0.4)
-    glVertex2f(0,80)   #verte
-    glVertex2f(-20,60)
-    glVertex2f(-20,10)
-    glVertex2f(-40,10)
-    glEnd()
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND); 
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     
     glBegin(GL_TRIANGLE_FAN)
-    glColor3f(0.0,1.2,0.2)
+    #1st triangle-top left corner
+    glVertex2f(-30,80)
+    glVertex2f(-60,80)
+    glVertex2f(-60,60)
+    glVertex2f(-40,60)
+    glVertex2f(-40,10)
+    glVertex2f(-20,10)
+    glVertex2f(-20,60)
+    glVertex2f(0,60)
+    glVertex2f(0,80)
+    glEnd()
+
+    glBegin(GL_TRIANGLE_FAN)
     glVertex2f(0,-10)
     glVertex2f(0,10)
     glVertex2f(-20,10)
-    glColor3f(1.0,0.0,0.4)
     glVertex2f(-40,10)
     glVertex2f(-60,10)
     glVertex2f(-60,-10)
     glEnd()
-
-    glBegin(GL_TRIANGLES)
-    glVertex2f(-20,60)
-    glVertex2f(0,80)
-    glVertex2f(0,60)
-    glEnd()
-
     #PLOT L
-    glBegin(GL_TRIANGLE_FAN)
+    '''glBegin(GL_TRIANGLE_FAN)
     glVertex2f(40,10)
     glVertex2f(70,10)
     glVertex2f(70,-10)
     glVertex2f(20,-10)
     glVertex2f(20,80)
     glVertex2f(40,80)
-    glEnd()
-    
+    glEnd()'''
     glFlush()
+    glutSwapBuffers() #dwap front/back framebuffer to avoid flickering
+
+
+    
+def reshape(width,height):
+    glViewport(0,0,width,height)
+    Aspect=float(width)/float(height)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(70.0,Aspect,0.001,30.0)
+    
+def timer(v):
+    global RotateAngle
+    RotateAngle+=1.0
+    glutPostRedisplay()
+    glutTimerFunc(1000/nFPS,timer,v)
 
 def main():
     glutInit(sys.argv)
-    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB)
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB )
     glutInitWindowSize(500,500)
-    glutInitWindowPosition(50,50)
-    glutCreateWindow("Plot Points")
-    glutDisplayFunc(plotpoints)
+    glutInitWindowPosition(100,100)
+    glutCreateWindow("MP1")
+    glutDisplayFunc(plotpoints) #drawing the I
+    glutKeyboardFunc(keyboard)
+    #glutReshapeFunc(reshape)
+    glutTimerFunc(100,timer,nFPS) #timer- to update animation 
     init()
     glutMainLoop()
 
